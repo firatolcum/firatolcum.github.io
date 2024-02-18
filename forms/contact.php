531ecd873953b2ee274f6cf021ee41ce9575561a
firatolcum <firatolcum@anadolu.edu.tr>
2024-02-18 17:43:32 +0300

    update

--- forms/contact.php
+++ forms/contact.php
@@ -28,7 +28,7 @@
   $contact->smtp = array(
     'host' => 'smtp.gmail.com',
     'username' => 'firatolcum@gmail.com',
-    'password' => 'tyjzvychvlvhqxuq',
+    'password' => '------------',
     'port' => '587'
   );
   
